b6c4e779eadff6ad2340720efad1ea8c6d9a3a21
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2025-04-25 03:09:50 +0000

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tech_taste/data/restaurants_data.dart';
 import 'package:tech_taste/ui/_core/app_theme.dart';
 import 'package:tech_taste/ui/splash/splash_screen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //Esse comando permite criar uma main assincrona
+  //List<Restaurant> listRestaurant = await RestaurantsData().getRestaurants();
+  //print(listRestaurant);
+  RestaurantsData restaurantData = RestaurantsData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tech_taste/data/categories_data.dart';
+import 'package:tech_taste/data/restaurants_data.dart';
+import 'package:tech_taste/model/restaurant.dart';
+import 'package:tech_taste/ui/home/widgets/category_widget.dart';
+import 'package:tech_taste/ui/home/widgets/restaurant_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    RestaurantsData restaurantData = Provider.of<RestaurantsData>(context);
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 32.0,
+            children: [
+              Center(child: Image.asset('assets/logo.png', width: 147)),
+              Text("Boas Vindas!"),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "O que você quer comer?",
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+              Text("Escolha por categoria"),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 8.0,
+                  children: List.generate(
+                    CategoriesData.listCategories.length,
+                    (index) {
+                      return CategoryWidget(
+                        category: CategoriesData.listCategories[index],
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Image.asset('assets/banners/banner_promo.png'),
+              Text("Bem avaliados"),
+              Column(
+                spacing: 12.0,
+                children: List.generate(restaurantData.listRestaurant.length, (
+                  index,
+                ) {
+                  Restaurant restaurant = restaurantData.listRestaurant[index];
+                  return RestaurantWidget(restaurant: restaurant);
+                }),
+              ),
+              SizedBox(
+                height: 64,
+              ), // Boa prática para espaçamento do texto no final
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
